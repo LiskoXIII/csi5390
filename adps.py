cd9d7de53159d4018ecaaea8759e30cdf3ba60ee
Lisko <lisko34@gmail.com>
2024-11-15 23:19:12 -0500

    added dataset description

--- adps.py
+++ adps.py
@@ -1,13 +1,68 @@
 import sys
+from dataclasses import dataclass
 
 from ui.ADPS import Ui_ADPS
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 
+@dataclass
+class Dataset:
+
+    # Patient Information
+    # Patient ID
+    PatientID: int = None
+    # Demographic Details
+    Age: int = None
+    Gender: int = None
+    Ethnicity: int = None
+    EducationLevel: int = None
+    # Lifestyle Factors
+    BMI = None
+    Smoking = None
+    AlcoholConsumption = None
+    PhysicalActivity = None
+    DietQuality = None
+    SleepQuality = None
+    # Medical History
+    FamilyHistoryAlzheimers = None
+    CardiovascularDisease = None
+    Diabetes = None
+    Depression = None
+    HeadInjury = None
+    Hypertension = None
+    # Clinical Measurements
+    SystolicBP = None
+    DiastolicBP = None
+    CholesterolTotal = None
+    CholesterolLDL = None
+    CholesterolHDL = None
+    CholesterolTriglycerides = None
+    # Cognitive and Functional Assessments
+    MMSE = None
+    FunctionalAssessment = None
+    MemoryComplaints = None
+    BehavioralProblems = None
+    ADL = None
+    # Symptoms
+    Confusion = None
+    Disorientation = None
+    PersonalityChanges = None
+    DifficultyCompletingTasks = None
+    Forgetfulness = None
+    # Diagnosis Information
+    Diagnosis = None
+    # Confidential Information
+    DoctorInCharge = None
+
+
 class Patient:
 
     def __init__(self):
-        self.result: Result = None
+        self.result: Result = Result()
+
+    
+
+    
 
 
 class Result:
