6e736929a99709743a98d29166a46410cb61477a
D-Georgi <david.georgi00@gmail.com>
2024-11-21 17:28:50 -0500

    model trained, new predict.py script used to make diagnosis. save patient data to patient_data.csv then call predict.py

--- prediction/model.py
+++ prediction/model.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import classification_report
 from catboost import CatBoostClassifier
+import joblib
 
 
 def preprocessing():
@@ -23,29 +24,38 @@ def preprocessing():
                'DiastolicBP', 'CholesterolTotal', 'CholesterolLDL', 'CholesterolHDL', 'CholesterolTriglycerides',
                'MMSE', 'FunctionalAssessment', 'ADL']
 
-    # normalize the columns
+    # Initialize scalers
     min_max_scaler = MinMaxScaler()
-    df[columns] = min_max_scaler.fit_transform(df[columns])
-
-    # standardize the columns
     standard_scaler = StandardScaler()
+
+    # Fit and transform the columns
+    df[columns] = min_max_scaler.fit_transform(df[columns])
     df[columns] = standard_scaler.fit_transform(df[columns])
+
+    # save scalars used in preprocessing for use in prediction
+    joblib.dump(min_max_scaler, "min_max_scaler.joblib")
+    joblib.dump(standard_scaler, "standard_scaler.joblib")
+    print("Scalers saved as 'min_max_scaler.joblib' and 'standard_scaler.joblib'.")
 
     return df
 
+
 def build_model(df):
-    # split data into features and target
     X = df.drop(columns=['Diagnosis'])
     y = df['Diagnosis']
 
-    # split data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
-
-    cat = CatBoostClassifier(iterations=100, learning_rate=0.01)
+    cat = CatBoostClassifier(iterations=100, learning_rate=0.01, verbose=0)
     cat.fit(X_train, y_train)
     y_pred = cat.predict(X_test)
+
+    # Print classification report
     report = classification_report(y_test, y_pred)
     print(report)
+
+    # save the trained model for prediction
+    cat.save_model("catboost_model.cbm")
+    print("Model saved as 'catboost_model.cbm'.")
 
 if __name__ == '__main__':
     data = preprocessing()
