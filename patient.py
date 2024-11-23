0f21ea9ea219bfb6e3e989d4c931aeed16aa4e04
Viljo Wagner <viljo.wagner@continental-corporation.com>
2024-11-23 01:13:20 -0500

    added data and results

--- patient.py
+++ patient.py
@@ -1,7 +1,10 @@
 from typing import Any
 
+from dataset import Dataset
+
 class Patient:
 
     def __init__(self, id: Any = None):
         self.id: Any = id
-        
+        self.data = Dataset()
+        self.result: Any = None
