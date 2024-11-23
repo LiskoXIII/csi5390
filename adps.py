b7bf7d1b4347b0815601344704113484698b3438
Viljo Wagner <viljo.wagner@continental-corporation.com>
2024-11-23 01:10:42 -0500

    initial ui integration

--- adps.py
+++ adps.py
@@ -1,37 +1,64 @@
 import sys
-from dataclasses import dataclass
 
-from ui.ADPS import Ui_ADPS
+from patient import Patient
+from ui.ui_adps import Ui_MainWindow
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 
-class Patient:
-
-    def __init__(self):
-        self.result: Result = Result()
-
-    
-
-    
-
-
-class Result:
-
-    def __init__(self):
-        pass
-
-
-class ADPS(QMainWindow, Ui_ADPS):
+class ADPS(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.connect_ui(self)
+        self.connect_ui()
+        self.widgetClinical.setVisible(False)
+        self.widgetCognitive.setVisible(False)
+        self.widgetResult.setVisible(False)
+        self.widgetHomepage.setVisible(False)
+        self.widgetPatient.setVisible(False)
+
+        self.patient = None
 
 
     def connect_ui(self):
-        pass
+        self.loginSubmit.clicked.connect(self.login_submit)
+        self.cognitive_button.clicked.connect(self.cognitive)
+        self.genetic_button.clicked.connect(self.clinical)
+        self.patient_button.clicked.connect(self.patient)
+        self.results_button.clicked.connect(self.result)
+
+    def login_submit(self):
+        self.patient = Patient(self.loginPatientID.text())
+        self.statusbar.showMessage(f"Logged in as {self.patient.id}")
+        self.widgetHomepage.setEnabled(True)
+        self.widgetHomepage.setVisible(True)
+        self.widgetLogin.setVisible(False)
+        self.widgetLogin.setEnabled(False)
+
+    def cognitive(self):
+        self.widgetCognitive.setEnabled(True)
+        self.widgetCognitive.setVisible(True)
+        self.widgetHomepage.setVisible(False)
+        self.widgetHomepage.setEnabled(False)
+
+    def clinical(self):
+        self.widgetClinical.setEnabled(True)
+        self.widgetClinical.setVisible(True)
+        self.widgetHomepage.setVisible(False)
+        self.widgetHomepage.setEnabled(False)
+
+    def patient(self): 
+        self.widgetPatient.setEnabled(True)
+        self.widgetPatient.setVisible(True)
+        self.widgetHomepage.setVisible(False)
+        self.widgetHomepage.setEnabled(False)
+
+    def result(self):
+        self.widgetResult.setEnabled(True)
+        self.widgetResult.setVisible(True)
+        self.widgetHomepage.setVisible(False)
+        self.widgetHomepage.setEnabled(False)
 
 
 def main():
---
+++ patient.py
@@ -0,0 +1,7 @@
+from typing import Any
+
+class Patient:
+
+    def __init__(self, id: Any = None):
+        self.id: Any = None
+        
