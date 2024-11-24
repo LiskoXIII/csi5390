cd5b58a436655d0b268c06b06f022b359d89f8ae
Viljo Wagner <viljo.wagner@continental-corporation.com>
2024-11-24 17:10:13 -0500

    added documentation

--- patient.py
+++ patient.py
@@ -5,6 +5,23 @@ from dataset import Dataset
 from pandas import DataFrame
 
 class Patient:
+    """
+    A class to represent a patient.
+    Attributes
+    ----------
+    id : Any, optional
+        The unique identifier for the patient (default is None).
+    data : Dataset
+        The dataset associated with the patient.
+    _result : DataFrame, optional
+        The result dataframe containing diagnosis prediction (default is None).
+    Methods
+    -------
+    result:
+        Property that gets the diagnosis prediction from the result dataframe.
+    result(value: DataFrame):
+        Setter that sets the result dataframe.
+    """
 
     def __init__(self, id: Any = None):
         self.id: Any = id
